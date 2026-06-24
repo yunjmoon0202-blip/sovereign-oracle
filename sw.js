78b8c7049fc76778b8fddffa3dda3f9bc3b6f29c
yunjmoon0202-blip <yunjmoon0202@gmail.com>
2026-06-24 20:53:24 +0900

    feat: Service Worker 별도 파일 추가

---
+++ sw.js
@@ -0,0 +1,7 @@
+const CACHE = 'so-v26';
+self.addEventListener('install', e => e.waitUntil(
+  caches.open(CACHE).then(c => c.addAll(['/']))
+));
+self.addEventListener('fetch', e => e.respondWith(
+  caches.match(e.request).then(r => r || fetch(e.request))
+));
